498279d96a837a29f26bc04202294ea144a86a58
Ni Ken <dattruongthe@gmail.com>
2022-06-22 17:35:58 +0700

    1. Add php bin path option 2. Add memory_limit option

--- safe_deploy.php
+++ safe_deploy.php
@@ -1,4 +1,5 @@
 <?php
+unset($argv[0]);
 
 $rootDir = getcwd();
 
@@ -10,33 +11,49 @@ $staticContentFolders = [
 
 $isFrontEnd = false;
 $isAdminhtml = false;
-foreach ($argv as $arg) {
+$phpVersion = 'php';
+$memoryLimit = '';
+foreach ($argv as $key => $arg) {
     if ( $arg == 'frontend' ) {
         $isFrontEnd = true;
     }
     if ( $arg == 'adminhtml' ) {
         $isAdminhtml = true;
     }
+    if (strpos($arg, 'php') !== false) {
+        unset($argv[$key]);
+        $bin = explode('=',$arg);
+        $phpVersion = $bin[1];
+    }
+    if (strpos($arg, 'php') !== false) {
+        unset($argv[$key]);
+        $bin = explode('=',$arg);
+        $phpVersion = $bin[1];
+    }
+    if (strpos($arg, 'memory_limit') !== false) {
+        unset($argv[$key]);
+        $memoryLimit = $arg;
+    }
 }
+
 if($isFrontEnd) {
-    $staticContentFolders[] = $rootDir.'/pub/static/frontend/*';
+    $staticContentFolders[] = $rootDir.'/pub/static/frontend';
 }
 if($isAdminhtml) {
-    $staticContentFolders[] = $rootDir.'/pub/static/adminhtml/*';
+    $staticContentFolders[] = $rootDir.'/pub/static/adminhtml';
 }
 if(!$isFrontEnd && !$isAdminhtml) {
-    $staticContentFolders[] = $rootDir.'/pub/static/*';
+    $staticContentFolders[] = $rootDir.'/pub/static';
 }
 
 foreach ($staticContentFolders as $staticContentFolder) {
     if(is_dir($staticContentFolder)) {
-        $commands[] = "rm -rf ".$staticContentFolder;
+        $commands[] = "rm -rf ".$staticContentFolder.'/*';
     }
 }
 
-unset($argv[0]);
 $arguments = implode(' ',$argv);
-$commands[] = $rootDir."/bin/magento"." s:s:d -f ".$arguments;
+$commands[] = $phpVersion." ".$memoryLimit." ".$rootDir."/bin/magento"." s:s:d -f ".$arguments;
 
 foreach ($commands as $command) {
     while (@ ob_end_flush());
